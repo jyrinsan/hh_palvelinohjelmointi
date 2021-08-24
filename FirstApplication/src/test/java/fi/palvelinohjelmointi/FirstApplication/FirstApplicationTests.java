a446e44af77d9501bbccbd5c7300f48d1d4ff7d6
Sanna Jyrinki <AA8206@student.jamk.fi>
2021-08-24 20:31:02 +0300

    first application

---
+++ FirstApplication/src/test/java/fi/palvelinohjelmointi/FirstApplication/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.palvelinohjelmointi.FirstApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
