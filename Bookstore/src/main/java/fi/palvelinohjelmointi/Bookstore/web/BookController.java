2deae80f3b34620c858764f91c109796bb9c7e88
Sanna Jyrinki <AA8206@student.jamk.fi>
2021-09-24 22:46:53 +0300

    rest harjoitukset

--- Bookstore/src/main/java/fi/palvelinohjelmointi/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/fi/palvelinohjelmointi/Bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package fi.palvelinohjelmointi.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.palvelinohjelmointi.Bookstore.domain.Book;
 import fi.palvelinohjelmointi.Bookstore.domain.BookRepository;
@@ -26,6 +30,18 @@ public class BookController {
   
         return "booklist";
     }
+	
+	// RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) bookRepository.findAll();
+    }    
+
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return bookRepository.findById(bookId);
+    }    
 	
     @RequestMapping(value = "/add")
     public String addBook(Model model){
